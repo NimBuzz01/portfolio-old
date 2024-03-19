491d7e09da6cf09c8cccdb85f83288275708e3d1
niamatmarjan <86093827+NimBuzz01@users.noreply.github.com>
2024-03-20 01:47:53 +0530

    update intro ui

--- components/intro.tsx
+++ components/intro.tsx
@@ -10,79 +10,104 @@ import { FaGithubSquare } from "react-icons/fa";
 import { useSectionInView } from "@/lib/hooks";
 import { useActiveSectionContext } from "@/context/active-section-context";
 
+const words1 = ["Hi", "There,", "I'm"];
+const words2 = ["Niamat", "Marjan."];
+const description: string = `A passionate frontend developer & web designer propelling
+visions to reality`;
+
 export default function Intro() {
   const { ref } = useSectionInView("Home", 0.5);
   const { setActiveSection, setTimeOfLastClick } = useActiveSectionContext();
+
+  const letterVariants = {
+    hidden: { opacity: 0, y: 50 },
+    visible: (i: number) => ({
+      opacity: 1,
+      y: 0,
+      transition: {
+        delay: i * 0.05 + 2.8,
+        duration: 0.3,
+        type: "spring",
+        stiffness: 100,
+        damping: 20,
+      },
+    }),
+  };
+
+  const textVariants = {
+    hidden: { opacity: 0, y: 50 },
+    visible: {
+      opacity: 1,
+      y: 0,
+      transition: {
+        delay: 3.5,
+        duration: 2,
+        type: "spring",
+        stiffness: 500,
+        damping: 30,
+      },
+    },
+  };
+
+  const renderText = (words: string[], startDelay: number = 0) =>
+    words.map((word, i) => (
+      <React.Fragment key={i}>
+        {word.split("").map((char, j) => (
+          <motion.span
+            key={j}
+            custom={startDelay + i + j * 0.05}
+            variants={letterVariants}
+            className="tracking-tighter"
+          >
+            {char}
+          </motion.span>
+        ))}
+        {i < words.length - 1 && <span>&nbsp;</span>}
+      </React.Fragment>
+    ));
 
   return (
     <section
       ref={ref}
       id="home"
-      className="mb-28 lg:mt-28 max-w-[50rem] text-center sm:mb-0 scroll-mt-[100rem]"
+      className="mb-28 lg:mt-24 max-w-[50rem] text-center sm:mb-0 scroll-mt-[100rem]"
     >
-      <div className="flex items-center justify-center">
-        <div className="relative">
-          <motion.div
-            initial={{ opacity: 0, scale: 0 }}
-            animate={{ opacity: 1, scale: 1 }}
-            transition={{
-              type: "tween",
-              duration: 0.2,
-              delay: 3,
-            }}
-          >
-            <Image
-              src="https://images.unsplash.com/photo-1507003211169-0a1dd7228f2d?fit=crop&w=368&h=368&q=100"
-              alt="Profile portrait"
-              width="192"
-              height="192"
-              quality="95"
-              priority={true}
-              className="h-24 w-24 rounded-full object-cover border-[0.35rem] border-white shadow-xl"
-            />
-          </motion.div>
-
-          <motion.span
-            className="absolute bottom-0 right-0 text-4xl"
-            initial={{ opacity: 0, scale: 0 }}
-            animate={{ opacity: 1, scale: 1 }}
-            transition={{
-              type: "spring",
-              stiffness: 125,
-              delay: 3.1,
-              duration: 0.7,
-            }}
-          >
-            👋
-          </motion.span>
-        </div>
+      <div className="p-6 sm:flex z-10 sm:flex-col sm:items-center mt-auto sm:mt-0 w-full sm:text-center">
+        <motion.h1
+          className="flex text-4xl sm:text-5xl md:text-6xl lg-text-7xl xl:text-8xl 2xl:text-9xl uppercase font-extrabold sm:-translate-x-24 md:-translate-x-28 lg:-translate-x-32 xl:-translate-x-36"
+          initial="hidden"
+          animate="visible"
+        >
+          {renderText(words1)}
+        </motion.h1>
+        <motion.h1
+          className="flex text-4xl sm:text-5xl md:text-6xl lg-text-7xl xl:text-8xl 2xl:text-9xl uppercase font-extrabold sm:translate-x-6 md:translate-x-8 lg:translate-x-10 xl:translate-x-12"
+          initial="hidden"
+          animate="visible"
+        >
+          {renderText(words2)}
+        </motion.h1>
+        <motion.p
+          className=" text-start sm:text-center text-lg sm:text-xl md:text-2xl w-full leading-tight mt-3 sm:mt-10 sm:max-w-96 md:max-w-[34rem]"
+          initial="hidden"
+          animate="visible"
+          variants={textVariants}
+        >
+          {description}
+        </motion.p>
       </div>
 
-      <motion.h1
-        className="mb-10 mt-4 px-4 text-2xl font-medium !leading-[1.5] sm:text-4xl"
-        initial={{ opacity: 0, y: 100 }}
-        animate={{ opacity: 1, y: 0 }}
-        transition={{
-          delay: 3,
-        }}
-      >
-        <span className="font-bold">Hello, I'm Niamat.</span> I'm a full-stack
-        web developer with <span className="font-bold">2 years</span> of
-        experience. I enjoy building sites & apps using Next.js, Typescript,
-        Tailwind CSS and Strapi .
-      </motion.h1>
-
       <motion.div
-        className="flex flex-wrap items-center justify-center gap-2 px-4 text-lg font-medium sm:flex-row"
+        className="flex flex-col items-center justify-center gap-2 px-4 text-lg font-medium sm:flex-row"
         initial={{ opacity: 0, y: 100 }}
         animate={{ opacity: 1, y: 0 }}
         transition={{
-          delay: 3.1,
+          delay: 3.6,
         }}
       >
         <Link
           href="#contact"
-          className="flex items-center gap-2 py-3 text-white transition bg-gray-900 rounded-full outline-none group px-7 focus:scale-110 hover:scale-110 hover:bg-gray-950 active:scale-105"
+          className="flex min-w-[11rem] justify-center items-center gap-2 py-3 text-white text-sm sm:text-base transition bg-gray-900 rounded-full outline-none group px-7 focus:scale-110 hover:scale-110 hover:bg-gray-950 active:scale-105"
           onClick={() => {
             setActiveSection("Contact");
             setTimeOfLastClick(Date.now());
@@ -93,28 +118,29 @@ export default function Intro() {
         </Link>
 
         <a
-          className="flex items-center gap-2 py-3 transition bg-white rounded-full outline-none cursor-pointer group px-7 focus:scale-110 hover:scale-110 active:scale-105 borderBlack dark:bg-white/10"
+          className="flex min-w-[11rem] justify-center text-sm sm:text-base items-center gap-2 py-3 transition bg-white rounded-full outline-none cursor-pointer group px-7 focus:scale-110 hover:scale-110 active:scale-105 borderBlack dark:bg-white/10"
           href="/niamatmarjan-cv.pdf"
           download
         >
           Download CV <HiDownload className="transition opacity-60" />
         </a>
+        <div className="flex gap-2 items-center justify-center">
+          <a
+            className="bg-white p-4 text-gray-700 hover:text-gray-950 flex items-center gap-2 rounded-full focus:scale-[1.15] hover:scale-[1.15] active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10 dark:text-white/60"
+            href="https://www.linkedin.com/in/niamatm/"
+            target="_blank"
+          >
+            <BsLinkedin />
+          </a>
 
-        <a
-          className="bg-white p-4 text-gray-700 hover:text-gray-950 flex items-center gap-2 rounded-full focus:scale-[1.15] hover:scale-[1.15] active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10 dark:text-white/60"
-          href="https://www.linkedin.com/in/niamatm/"
-          target="_blank"
-        >
-          <BsLinkedin />
-        </a>
-
-        <a
-          className="bg-white p-4 text-gray-700 flex items-center gap-2 text-[1.35rem] rounded-full focus:scale-[1.15] hover:scale-[1.15] hover:text-gray-950 active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10 dark:text-white/60"
-          href="https://github.com/NimBuzz01"
-          target="_blank"
-        >
-          <FaGithubSquare />
-        </a>
+          <a
+            className="bg-white justify-center p-4 text-gray-700 flex items-center gap-2 text-[1.35rem] rounded-full focus:scale-[1.15] hover:scale-[1.15] hover:text-gray-950 active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10 dark:text-white/60"
+            href="https://github.com/NimBuzz01"
+            target="_blank"
+          >
+            <FaGithubSquare />
+          </a>
+        </div>
       </motion.div>
     </section>
   );
