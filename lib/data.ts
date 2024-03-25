e952b52b6039a7af9cc6b9d93f8db98ecd72a56a
niamatmarjan <86093827+NimBuzz01@users.noreply.github.com>
2024-03-25 14:53:15 +0530

    remove design placeholders

--- lib/data.ts
+++ lib/data.ts
@@ -119,81 +119,63 @@ export const skillsData = [
 
 export const galleryItems = [
   {
-    title: "Moonbeam",
-    thumbnail:
-      "https://aceternity.com/images/products/thumbnails/new/moonbeam.png",
+    title: "Coming Soon",
+    thumbnail: "/designs/coming-soon.png",
   },
   {
-    title: "Cursor",
-    thumbnail:
-      "https://aceternity.com/images/products/thumbnails/new/cursor.png",
+    title: "Coming Soon",
+    thumbnail: "/designs/coming-soon.png",
   },
   {
-    title: "Rogue",
-    thumbnail:
-      "https://aceternity.com/images/products/thumbnails/new/rogue.png",
-  },
-
-  {
-    title: "Editorially",
-    thumbnail:
-      "https://aceternity.com/images/products/thumbnails/new/editorially.png",
+    title: "Coming Soon",
+    thumbnail: "/designs/coming-soon.png",
   },
   {
-    title: "Editrix AI",
-    thumbnail:
-      "https://aceternity.com/images/products/thumbnails/new/editrix.png",
+    title: "Coming Soon",
+    thumbnail: "/designs/coming-soon.png",
   },
   {
-    title: "Pixel Perfect",
-    thumbnail:
-      "https://aceternity.com/images/products/thumbnails/new/pixelperfect.png",
-  },
-
-  {
-    title: "Algochurn",
-    thumbnail:
-      "https://aceternity.com/images/products/thumbnails/new/algochurn.png",
+    title: "Coming Soon",
+    thumbnail: "/designs/coming-soon.png",
   },
   {
-    title: "Aceternity UI",
-    thumbnail:
-      "https://aceternity.com/images/products/thumbnails/new/aceternityui.png",
+    title: "Coming Soon",
+    thumbnail: "/designs/coming-soon.png",
   },
   {
-    title: "Tailwind Master Kit",
-    thumbnail:
-      "https://aceternity.com/images/products/thumbnails/new/tailwindmasterkit.png",
+    title: "Coming Soon",
+    thumbnail: "/designs/coming-soon.png",
   },
   {
-    title: "SmartBridge",
-    thumbnail:
-      "https://aceternity.com/images/products/thumbnails/new/smartbridge.png",
+    title: "Coming Soon",
+    thumbnail: "/designs/coming-soon.png",
   },
   {
-    title: "Renderwork Studio",
-    thumbnail:
-      "https://aceternity.com/images/products/thumbnails/new/renderwork.png",
-  },
-
-  {
-    title: "Creme Digital",
-    thumbnail:
-      "https://aceternity.com/images/products/thumbnails/new/cremedigital.png",
+    title: "Coming Soon",
+    thumbnail: "/designs/coming-soon.png",
   },
   {
-    title: "Golden Bells Academy",
-    thumbnail:
-      "https://aceternity.com/images/products/thumbnails/new/goldenbellsacademy.png",
+    title: "Coming Soon",
+    thumbnail: "/designs/coming-soon.png",
   },
   {
-    title: "Invoker Labs",
-    thumbnail:
-      "https://aceternity.com/images/products/thumbnails/new/invoker.png",
+    title: "Coming Soon",
+    thumbnail: "/designs/coming-soon.png",
   },
   {
-    title: "E Free Invoice",
-    thumbnail:
-      "https://aceternity.com/images/products/thumbnails/new/efreeinvoice.png",
+    title: "Coming Soon",
+    thumbnail: "/designs/coming-soon.png",
+  },
+  {
+    title: "Coming Soon",
+    thumbnail: "/designs/coming-soon.png",
+  },
+  {
+    title: "Coming Soon",
+    thumbnail: "/designs/coming-soon.png",
+  },
+  {
+    title: "Coming Soon",
+    thumbnail: "/designs/coming-soon.png",
   },
 ];
